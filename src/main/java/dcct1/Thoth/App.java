7b4516aa0f19aeb8b240dc47673c0d41cfc6e9ea
cct1 <declankennyhome@yahoo.co.uk>
2018-02-16 16:40:55 +0000

    git commit -m "initial commit"

---
+++ src/main/java/dcct1/Thoth/App.java
@@ -0,0 +1,13 @@
+package dcct1.Thoth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
