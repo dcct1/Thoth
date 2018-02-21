2810010171da11fe5e54c95ba083674d769a71b7
cct1 <declankennyhome@yahoo.co.uk>
2018-02-21 16:41:24 +0000

    App modified from default

--- src/main/java/dcct1/Thoth/App.java
+++ src/main/java/dcct1/Thoth/App.java
@@ -1,13 +1,84 @@
 package dcct1.Thoth;
 
+import java.util.Date;
+import java.util.Scanner;
+
+
 /**
- * Hello world!
- *
+ * Date: Feb 2018
+ * @author Declan.Kenny
+ * 
+ * The Purpose of this is to show an example of 
+ * 
+ * The use of development tools : GIT MAVEN ECLIPSE
+ * How to use Eclipse
+ * Provides a refresher OOP in Java
+ * Provide an introduction to project file structure layout - MAEN Archetype
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       //To view the arguments being created
+    	seeCommandlineInput(args);
+    	
+    	//to instantiate App class based in the parameters entered at the commandline
+    	actionCommandlineInput(args);
+    }
+    	//DATA
+    	// define attributes
+    
+    private Scanner someInput;
+    private Date today;
+    
+    
+    //Constructors
+    
+    public App()
+    {
+    	
+    this.someInput = new Scanner(System.in);
+    
+    //do something
+    System.out.println("\n Sooon.... stuff will happen here ");
+    
+    //pause before exit
+    System.out.println("\n Press enter to exit program");
+    this.someInput.nextLine();
+    
+    //close the program without error
+    System.exit(0);
+    
+    }
+    
+    /**
+     * action the arguments presented on the command line 
+     * instantiate the App class based on the arguments passed 
+     */
+    
+    public static void actionCommandlineInput (String args[]) 
+    {
+    	
+    	App anApp = new App();
+    	
+    }
+
+    public static void seeCommandlineInput (String args[])
+    {
+    	if (args.length == 0)
+    	{
+    		System.out.println("There was no commands passed");
+    		
+    	}
+    	else
+    	{
+    		//dispaly the command line text entered
+    		for(int i=0; i <args.length; i++)
+    		{
+    			System.out.println(args[i]);
+    		}
+    	}
     }
 }
+
+
