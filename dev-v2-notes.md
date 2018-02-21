f76de412f99542912e4cf979e94ca1c07c925838
cct1 <declankennyhome@yahoo.co.uk>
2018-02-21 18:50:14 +0000

    App modified from default

---
+++ dev-v2-notes.md
@@ -0,0 +1,6 @@
+## dev-v2 to do notes
+
+
+Some notes to work in this branch
+
+a reminder of the branch we are working in 
--- src/main/java/dcct1/Thoth/App.java
+++ src/main/java/dcct1/Thoth/App.java
@@ -2,7 +2,8 @@ package dcct1.Thoth;
 
 import java.util.Date;
 import java.util.Scanner;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Date: Feb 2018
@@ -31,11 +32,20 @@ public class App
     private Scanner someInput;
     private Date today;
     
+    private static Logger LOG;
+    
     
     //Constructors
     
     public App()
     {
+    //associate logging with this class so we know the message comes from this class
+    	
+    	LOG = LogManager.getLogger(App.class);
+    	
+    	//test the logging
+    	testLogOutput();
+    	
     	
     this.someInput = new Scanner(System.in);
     
@@ -78,6 +88,23 @@ public class App
     			System.out.println(args[i]);
     		}
     	}
+    }
+    
+    /**
+     * test the Log4j2 logging
+     */
+    
+    private static void testLogOutput()
+    {
+    	LOG.debug("Log test: Test printed info");
+    	LOG.info("Log test: Test printed info");
+    	LOG.warn("Log test: Test printed warn");
+    	LOG.error("Log test: Test printed error");
+    	LOG.fatal("Log test: Test printed fatal");
+    	
+    	LOG.info("Appending string: {}.", "Application log test message - hi ");
+    	
+    	
     }
 }
 
