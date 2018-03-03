777546cb1f1a2b51dd3f17b3bc5a57426dd46576
cct1 <declankennyhome@yahoo.co.uk>
2018-03-03 15:25:57 +0000

    Added Music menu item

--- src/main/java/dcct1/Thoth/music/MusicViewMenuItem.java
+++ src/main/java/dcct1/Thoth/music/MusicViewMenuItem.java
@@ -1,17 +1,22 @@
 package dcct1.Thoth.music;
 
-import	dcct1.Thoth.data.DataManagerSQLite;
-import	dcct1.Thoth.menu.IExecuatable;
- 
-public class MusicViewMenuItem implements IExecuatable 
-	{
-				
-	public	MusicViewMenuItem(){}
-				
-	public	void	execute()
-	{
+import java.util.Scanner;
+
+import dcct1.Thoth.menu.ConsoleControls;
+import dcct1.Thoth.menu.IExecuatable;
+
+public class MusicViewMenuItem implements IExecuatable {
+
+	Scanner input = new Scanner(System.in);
+	 
+	public MusicViewMenuItem() {}
+
+	public void execute() {
 		
-								
+		System.out.println( ConsoleControls.ANSI_YELLOW_BRIGHT + "... MUSIC VIEWER ..." + ConsoleControls.ANSI_RESET);
+		System.out.println("\n"+ MusicViewMenuItem.class.getName() + " has no MVC implementation.");
+        System.out.println(" \n Press enter return to the main menu");
+		this.input.nextLine();
 	}
-				
+
 }
